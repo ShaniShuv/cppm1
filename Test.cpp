830862c98ff1c0878a2194684201d0a2ddf320a9
ShaniShuv <abhauc97@gmail.com>
2021-03-06 14:56:30 +0200

    seprated tests fot each part:

--- Test.cpp
+++ Test.cpp
@@ -10,7 +10,14 @@ using namespace std;
 #define MAX8 44444444
 #endif
 
-int lengthofall [8][4] = {{5, 11, 11, 11}, {1, 1, 1, 0}, }
+int lengthofall [6][4] = {
+                            {5, 11, 11, 11}, // hat's lengths sorted bt cases
+                            {1, 1, 1, 0}, 
+                            {1, 1, 1, 1},  // although in all eye cases it's the same I rather this order 
+                            {1, 1, 1, 0}, 
+                            {3, 3, 3, 3},
+                            {3, 3, 3, 3}
+                          };
 // {0, 0, 0 , 0};
 // string hatcomp = "_===_";
 // lengthofall0 = ((hatcomp.length()); 
@@ -32,7 +39,8 @@ TEST_CASE("Good hat initialization"){
     int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
     string sms = snowmanCreation(smc); // snowman string
     string hatcomp = NULL; // a string to compare
-    int hat = smc / 10000000;
+    int div = pow(10, 8);
+    int hat = smc / div;
     switch (hat)
     {
     case 1:
@@ -62,27 +70,11 @@ TEST_CASE("Good nose initialization"){
     int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
     string sms = snowmanCreation(smc); // snowman string
     string nosecomp = NULL; // a string to compare
-     int hat = smc / 10000000;
-    // string hatcomp = NULL; // a string to compare
-    int nose = smc / 1000000;
+    int div = pow(10, 7);
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div;  
     nose %= 10;
-    // switch (hat)
-    // {
-    // case 1:
-    //     hatcomp = "_===_";
-    //     break;
-    // case 2:
-    //     hatcomp = " ___ \n.....";
-    //     break;
-    // case 3:
-    //     hatcomp = "  _  \n /_\ ";
-    //     break;
-    // case 4:
-    //     hatcomp = " ___ \n(_*_)";
-    //     break;
-    // default:
-    //     CHECK(false);
-    // }
     int sofarlength = lengthofall[0][hat]; //hatcomp.length(); 
 
     switch (nose)
@@ -103,45 +95,118 @@ TEST_CASE("Good nose initialization"){
     }
 }
 
-TEST_CASE("Good nose initialization"){
+TEST_CASE("Good left eye initialization"){
     int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
     string sms = snowmanCreation(smc); // snowman string
     string nosecomp = NULL; // a string to compare
-    int hat = smc / 10000000;
-    // string hatcomp = NULL; // a string to compare
-    int nose = smc / 1000000;
-    nose %= 10;
-    int nose = smc / 1000000;
-    nose %= 10;
-    // switch (hat)
-    // {
-    // case 1:
-    //     hatcomp = "_===_";
-    //     break;
-    // case 2:
-    //     hatcomp = " ___ \n.....";
-    //     break;
-    // case 3:
-    //     hatcomp = "  _  \n /_\ ";
-    //     break;
-    // case 4:
-    //     hatcomp = " ___ \n(_*_)";
-    //     break;
-    // default:
-    //     CHECK(false);
-    // }
+    int div = pow(10, 6);
+    int lefteye = smc / div;
+    div *= 10;
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div; 
+
+    int mul = 10;
+    lefteye %= mul;
+    mul *= 10;
+    nose %= mul;
+
     int sofarlength = lengthofall[0][hat] + lengthofall[1][nose]; //hatcomp.length(); 
 
-    switch (nose)
+    switch (lefteye)
     {
     case 1:
-        CHECK((sms[sofarlength] == ','));
-        break;
-    case 2:
         CHECK((sms[sofarlength] == '.'));
         break;
+    case 2:
+        CHECK((sms[sofarlength] == 'o'));
+        break;
     case 3:
-        CHECK((sms[sofarlength] == '_'));
+        CHECK((sms[sofarlength] == 'O'));
+        break;
+    case 4:
+        CHECK((sms[sofarlength] == '-'));
+        break;
+    default:
+        CHECK(false);
+    }
+}
+
+TEST_CASE("Good right eye initialization"){
+    int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
+    string sms = snowmanCreation(smc); // snowman string
+    string nosecomp = NULL; // a string to compare
+    int div = pow(10, 5);
+    int righteye = smc / div;
+    div *= 10;
+    int lefteye = smc / div;
+    div *= 10;
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div; 
+
+    int mul = 10;
+    righteye %= mul;
+    mul *= 10;
+    lefteye %= mul;
+    mul *= 10;
+    nose %= mul;
+    int sofarlength = lengthofall[0][hat] + lengthofall[1][nose] + lengthofall[2][lefteye]; //hatcomp.length(); 
+
+    switch (righteye)
+    {
+    case 1:
+        CHECK((sms[sofarlength] == '.'));
+        break;
+    case 2:
+        CHECK((sms[sofarlength] == 'o'));
+        break;
+    case 3:
+        CHECK((sms[sofarlength] == 'O'));
+        break;
+    case 4:
+        CHECK((sms[sofarlength] == '-'));
+        break;
+    default:
+        CHECK(false);
+    }
+}
+
+TEST_CASE("Good left arm initialization"){
+    int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
+    string sms = snowmanCreation(smc); // snowman string
+    string nosecomp = NULL; // a string to compare
+    int div = pow(10, 4);
+    int leftarm = smc / div;
+    div *= 10;
+    int righteye = smc / div;
+    div *= 10;
+    int lefteye = smc / div;
+    div *= 10;
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div; 
+
+    int mul = 10;
+    leftarm %= mul;
+    mul *= 10;
+    righteye %= mul;
+    mul *= 10;
+    lefteye %= mul;
+    mul *= 10;
+    nose %= mul;
+    int sofarlength = lengthofall[0][hat] + lengthofall[1][nose] + lengthofall[2][lefteye] + lengthofall[2][righteye]; //hatcomp.length(); 
+
+    switch (leftarm)
+    {
+    case 1:
+        CHECK((sms[sofarlength] == '<'));
+        break;
+    case 2:
+        CHECK((sms[sofarlength] == '\\'));
+        break;
+    case 3:
+        CHECK((sms[sofarlength] == '\/'));
         break;
     case 4:
         break;
@@ -150,16 +215,187 @@ TEST_CASE("Good nose initialization"){
     }
 }
 
+TEST_CASE("Good right arm initialization"){
+    int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
+    string sms = snowmanCreation(smc); // snowman string
+    string nosecomp = NULL; // a string to compare
+    int div = pow(10, 3);
+    int rightarm = smc / div;
+    div *= 10;
+    int leftarm = smc / div;
+    div *= 10;
+    int righteye = smc / div;
+    div *= 10;
+    int lefteye = smc / div;
+    div *= 10;
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div; 
 
-TEST_CASE("Good snowman code") {
-    CHECK(nospaces(snowman(11114411)) == nospaces("_===_\n(.,.)\n( : )\n( : )"));
-    /* Add more checks here */
+    int mul = 10;
+    rightarm %= mul;
+    mul *= 10;
+    leftarm %= mul;
+    mul *= 10;
+    righteye %= mul;
+    mul *= 10;
+    lefteye %= mul;
+    mul *= 10;
+    nose %= mul;
+    int sofarlength = lengthofall[0][hat] + lengthofall[1][nose] + lengthofall[2][lefteye] + lengthofall[2][righteye] + lengthofall[3][leftarm]; //hatcomp.length(); 
+
+    switch (rightarm)
+    {
+    case 1:
+        CHECK((sms[sofarlength] == '>'));
+        break;
+    case 2:
+        CHECK((sms[sofarlength] == '\/'));
+        break;
+    case 3:
+        CHECK((sms[sofarlength] == '\\'));
+        break;
+    case 4:
+        break;
+    default:
+        CHECK(false);
+    }
 }
 
-TEST_CASE("Bad snowman code") {
-    CHECK_THROWS(snowman(555));
-    /* Add more checks here */
+TEST_CASE("Good torso initialization"){
+    int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
+    string sms = snowmanCreation(smc); // snowman string
+    string nosecomp = NULL; // a string to compare
+    int div = 100;
+    int torso = smc / div;
+    div *= 10;
+    int rightarm = smc / div;
+    div *= 10;
+    int leftarm = smc / div;
+    div *= 10;
+    int righteye = smc / div;
+    div *= 10;
+    int lefteye = smc / div;
+    div *= 10;
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div; 
+
+    int mul = 10;
+    torso %= mul;
+    mul *= 10;
+    rightarm %= mul;
+    mul *= 10;
+    leftarm %= mul;
+    mul *= 10;
+    righteye %= mul;
+    mul *= 10;
+    lefteye %= mul;
+    mul *= 10;
+    nose %= mul;
+    int sofarlength = lengthofall[0][hat] + lengthofall[1][nose] + lengthofall[2][lefteye] + lengthofall[2][righteye] + lengthofall[3][leftarm] + lengthofall[3][rightarm]; //hatcomp.length(); 
+
+    string torsocomp = NULL;
+
+    switch (torso)
+    {
+    case 1:
+        torsocomp = " : ";
+        break;
+    case 2:
+        torsocomp = "] [";
+        break;
+    case 3:
+        torsocomp = "> <";
+        break;
+    case 4:
+        torsocomp = "   ";
+        break;
+    default:
+        CHECK(false);
+    }
+
+    for (size_t i = 0; i < lengthofall[4][torso]; i++)
+    {
+        CHECK((sms[sofarlength+i] == torsocomp[i]));
+    }
+    
 }
+
+TEST_CASE("Good base initialization"){
+    int smc = rand()*(MAX8-MIN8) + MIN8; // snowman case
+    string sms = snowmanCreation(smc); // snowman string
+    // string nosecomp = NULL; // a string to compare
+    int div = 10;
+    int base = smc / div;
+    div *= 10;
+    int torso = smc / div;
+    div *= 10;
+    int rightarm = smc / div;
+    div *= 10;
+    int leftarm = smc / div;
+    div *= 10;
+    int righteye = smc / div;
+    div *= 10;
+    int lefteye = smc / div;
+    div *= 10;
+    int nose = smc / div;
+    div *= 10;
+    int hat = smc / div; 
+
+    int mul = 10;
+    base %= mul;
+    mul *= 10;
+    torso %= mul;
+    mul *= 10;
+    rightarm %= mul;
+    mul *= 10;
+    leftarm %= mul;
+    mul *= 10;
+    righteye %= mul;
+    mul *= 10;
+    lefteye %= mul;
+    mul *= 10;
+    nose %= mul;
+    int sofarlength = lengthofall[0][hat] + lengthofall[1][nose] + lengthofall[2][lefteye] + lengthofall[2][righteye] + lengthofall[3][leftarm] + lengthofall[3][rightarm] + lengthofall[5][torso]; //hatcomp.length(); 
+
+    string basecomp = NULL;
+
+    switch (base)
+    {
+    case 1:
+        basecomp = " : ";
+        break;
+    case 2:
+        basecomp = "] [";
+        break;
+    case 3:
+        basecomp = "> <";
+        break;
+    case 4:
+        basecomp = "   ";
+        break;
+    default:
+        CHECK(false);
+    }
+
+    for (size_t i = 0; i < lengthofall[4][torso]; i++)
+    {
+        CHECK((sms[sofarlength+i] == basecomp[i]));
+    }
+    
+}
+
+
+// TEST_CASE("Good snowman code") {
+//     CHECK(nospaces(snowman(11114411)) == nospaces("_===_\n(.,.)\n( : )\n( : )"));
+//     /* Add more checks here */
+// }
+
+// TEST_CASE("Bad snowman code") {
+//     CHECK_THROWS(snowman(555));
+//     /* Add more checks here */
+// }
 
 
 
